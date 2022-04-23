88f7f615feb99b872d439a3197120610446630eb
Heitor Saulo <heitor-081@outlook.com>
2022-04-22 23:52:21 -0300

    Primeiro commit

---
+++ src/sistema/BancoDeDados.java
@@ -0,0 +1,7 @@
+package sistema;
+
+public class BancoDeDados {
+    public static final String stringDeConexao = "jdbc:postgresql://localhost:5432/sistema_de_funcionarios";
+    public static final String usuario = "postgres";
+    public static final String senha = "Tars-081004";
+}
