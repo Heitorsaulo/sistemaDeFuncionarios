9e88ac1f5e64cea483133a344c472ee2e1d5546b
Heitor Saulo <heitor-081@outlook.com>
2022-04-25 10:50:19 -0300

    Data base changing file

--- src/sistema/BancoDeDados.java
+++ src/sistema/BancoDeDados.java
@@ -1,7 +1,11 @@
 package sistema;
 
 public class BancoDeDados {
-    public static final String stringDeConexao = "jdbc:postgresql://localhost:5432/sistema_de_funcionarios";
+    private static final String port = "5432";
+    private static final String host = "localhost";
+    private static final String db = "sistema_de_funcionarios";
+    public static final String stringDeConexao = "jdbc:postgresql://"+ host +":"+ port + "/"+ db;
     public static final String usuario = "postgres";
     public static final String senha = "Tars-081004";
+    
 }
